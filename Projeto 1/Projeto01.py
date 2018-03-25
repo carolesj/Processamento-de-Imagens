917699ecc4615b9767239965bb96f69dbad12bf5
carolesj <caroline.jesuino.silva@usp.br>
2018-03-25 19:32:29 -0300

    Adicionadas todas as funções.

--- Projeto 1/Projeto01.py
+++ Projeto 1/Projeto01.py
@@ -1,93 +1,143 @@
-'''
+"""
 Aluna: Caroline Jesuíno Nunes da Silva
 Numero USP: 9293925
 Disciplina: SCC0251
 Ano/semestre: 2018/1
 Trabalho 1 - Gerador de imagens
-'''
+"""
 
-'''
-Todo:
-    .random walk
-    .normalizar as matrizes geradas
-    .digitalização
-    .comparação
-'''
+
 import numpy as np
 import random as rand
 
-def le_matriz(nome_arquivo) :
-    #carrega uma imagem ao ler um nome de arquivo
-    #e remover possíveis caracteres em branco
+
+def le_matriz(nome_arquivo):
+    # carrega uma imagem ao ler um nome de arquivo
+    # e remover possíveis caracteres em branco
     img = np.load(nome_arquivo.rstrip())
     return img
 
-#f(x,y) = (x + y)
-def funcao1(tamC) :
-    gerada = np.zeros((tamC, tamC))
-    for i in range(tamC):
-        for j in range(tamC):
+
+# f(x,y) = (x + y)
+def funcao1(tamc):
+    gerada = np.zeros((tamc, tamc))
+    for i in range(tamc):
+        for j in range(tamc):
             gerada[i][j] = i + j
-    #matriz retornada com valores não normalizados
+    # matriz retornada com valores não normalizados
     return gerada
 
-#f(x,y) = |sen(x/Q) + sen(y/Q)|
-def funcao2(Q, tamC) :
-    gerada = np.zeros((tamC, tamC))
-    for i in range(tamC):
-        for j in range(tamC):
-            gerada[i][j] = np.absolute(np.sin(i/Q) + np.sin(j/Q))
+
+# f(x,y) = |sen(x/Q) + sen(y/Q)|
+def funcao2(q, tamc):
+    gerada = np.zeros((tamc, tamc))
+    for i in range(tamc):
+        for j in range(tamc):
+            gerada[i][j] = np.absolute(np.sin(i/q) + np.sin(j/q))
     return gerada
 
-#f(x,y) = [(x/Q) - sqrt(y/Q)]
-def funcao3(Q, tamC) :
-    gerada = np.zeros((tamC, tamC))
-    for i in range(tamC):
-        for j in range(tamC):
-            gerada[i][j] = (i/Q - np.sqrt(j/Q))
+
+# f(x,y) = [(x/Q) - sqrt(y/Q)]
+def funcao3(q, tamc):
+    gerada = np.zeros((tamc, tamc))
+    for i in range(tamc):
+        for j in range(tamc):
+            gerada[i][j] = (i/q - np.sqrt(j/q))
     return gerada
 
-#f(x,y) = rand(0, 1, S)
-def funcao4(tamC, S) :
-    rand.seed(S)
-    gerada = np.zeros((tamC, tamC))
-    for i in range(tamC):
-        for j in range(tamC):
+
+# f(x,y) = rand(0, 1, S)
+def funcao4(tamc, s):
+    rand.seed(s)
+    gerada = np.zeros((tamc, tamc))
+    for i in range(tamc):
+        for j in range(tamc):
             gerada[i][j] = rand.random()
     return gerada
 
-#f(x,y) = randomwalk(S)
-#número de passos = 1 + (C^2)/2
-def funcao5() :
-    gerada = 0
+
+# f(x,y) = randomwalk(S)
+# número de passos = 1 + (C^2)/2
+def funcao5(tamc, s):
+    i = 0
+    j = 0
+    rand.seed(s)
+    gerada = np.zeros((tamc, tamc))
+    for n in range(1 + (tamc**2)/2):
+        gerada[i][j] = 1
+        i += rand.randrange(-1, 2, 1)
+        j += rand.randrange(-1, 2, 1)
+        i = divmod(i, tamc)
+        j = divmod(j, tamc)
+
     return gerada
 
-#recebe a imagem gerada e a normaliza baseando-se no número
-#de bits por pixel informados na entrada
-def normalizaImagem(gerada, nbitspp) :
-    normalizada = 0
-    return normalizada
 
-def main() :
+# recebe a imagem gerada e a normaliza baseando-se no número
+# de bits por pixel informados na entrada
+def normaliza_imagem(tamc, gerada):
+    maximo = np.amax(gerada)
+    limite = 65535
+    for i in range(tamc):
+        for j in range(tamc):
+            gerada[i][j] = (gerada[i][j] / maximo) * limite
+    return gerada
+
+
+def digitaliza(normalizada, tamc, tamn, nbitspp):
+    digitalizada = np.zeros((tamn, tamn))
+    lado_matriz = int(tamc / tamn)
+    maximo = 65535
+    for i in range(tamn):
+        for j in range(tamn):
+            inicio_m = int(i * lado_matriz)
+            fim_m = inicio_m + lado_matriz
+            inicio_n = int(j * lado_matriz)
+            fim_n = inicio_m + lado_matriz
+            digitalizada[i][j] = np.amax(normalizada[inicio_m:fim_m,inicio_n:fim_n])
+            digitalizada[i][j] = digitalizada[i][j] * (2**nbitspp) / maximo
+    return digitalizada
+
+
+def compara(lida, digitalizada, tamn):
+    erro = 0
+    for i in range(tamn):
+        for j in range(tamn):
+            erro = erro + digitalizada[i][j] - lida[i][j]
+    erro = erro ** 2
+    erro = np.sqrt(erro)
+
+    return erro
+
+
+def main():
     nome_arquivo = input()
-    tamC = input()
+    tamc = int(input())
     tipo = int(input())
-    Q = input()
-    tamN = input()
-    nbitspp = input()
-    S = input()
+    q = int(input())
+    tamn = int(input())
+    nbitspp = int(input())
+    s = int(input())
 
     carregada = le_matriz(nome_arquivo)
 
     if tipo == 1:
-        gerada = funcao1(tamC)
+        gerada = funcao1(tamc)
     elif tipo == 2:
-        gerada = funcao2(Q, tamC)
+        gerada = funcao2(q, tamc)
     elif tipo == 3:
-        gerada = funcao3(Q, tamC)
+        gerada = funcao3(q, tamc)
     elif tipo == 4:
-        gerada = funcao4(tamC, S)
+        gerada = funcao4(tamc, s)
     else:
-        gerada = funcao5(carregada, tamC, Q, tamN, nbitspp, S)
+        gerada = funcao5(tamc, s)
 
-main()
+    normalizada = normaliza_imagem(tamc, gerada)
+    digitalizada = digitaliza(normalizada, tamc, tamn, nbitspp)
+    erro = compara(carregada, digitalizada, tamn)
+
+    print(erro)
+
+
+if __name__ == '__main__':
+    main()
