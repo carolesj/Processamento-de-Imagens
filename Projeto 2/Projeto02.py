8c806c265521acf2b045f27c3d48e29253aaf1f7
carolesj <caroline.jesuino.silva@usp.br>
2018-03-27 00:11:29 -0300

    Adicionadas funções de equalização baseadas em histograma

--- Projeto 2/Projeto02.py
+++ Projeto 2/Projeto02.py
@@ -1,22 +1,111 @@
 """
-Aluna: Caroline Jesuíno Nunes da Silva
-Número USP: 9293925
-Disciplina SCC0251 - Processamento de Imagens
-2018/1
-Trabalho 2 - Realce e Superresolução
+    Aluna: Caroline Jesuíno Nunes da Silva
+    Número USP: 9293925
+    Disciplina SCC0251 - Processamento de Imagens
+    2018/1
+    Trabalho 2 - Realce e Superresolução
 """
+'''
+    TO DO:
+        Ajuste gama
+        Superresolução
+        Cálculo do erro
+'''
+
+
 
 import imageio
 import numpy as np
 import matplotlib as plt
 
 
-def histograma_transf_individual(img1, img2, img3, img4):
-    return
+# calcula o histograma para uma imagem individualmente
+def calcula_histograma_individual(img):
+    histograma = np.zeros([256])
+    x, y = img.shape
+    for i in range(x):
+        for j in range(y):
+            # acessa o vetor que representa o histograma na posição
+            # descrita pelo valor de cada pixel da imagem e incrementa
+            # a contagem daquele valor
+            histograma[img[i][j]] += 1
+    return histograma
 
 
-def histograma_transf_conjunta(img1, img2, img3, img4):
-    return
+# calcula o histograma para as quatro imagens ao mesmo tempo
+# chamando a função de histograma individual e acumulando os
+# resultados todos no mesmo vetor
+def calcula_histograma_conjunto(img1, img2, img3, img4):
+    histograma = calcula_histograma_individual(img1)
+    histograma = np.add(histograma, calcula_histograma_individual(img2))
+    histograma = np.add(histograma, calcula_histograma_individual(img3))
+    histograma = np.add(histograma, calcula_histograma_individual(img4))
+    return histograma
+
+
+def _equaliza_histograma_individual(img):
+    histograma = calcula_histograma_individual(img)
+    # cria um histograma cumulativo no qual cada posição representa
+    # o número de pixels existentes até aquele determinado tom de cinza
+    histogramac = np.zeros(histograma.shape)
+    histogramac[0] = histograma[0]
+    tamanhoh = histograma.shape[0]
+    x, y = img.shape
+
+    # resolve as acumulações
+    for i in range(1, tamanhoh):
+        histogramac[i] = histograma[i] + histogramac[i - 1]
+
+    # cria uma matriz do tamanho da imagem original para ser a
+    # resultante, com valores inteiros de 8 bits
+    equalizada = np.zeros(img.shape).astype(np.uint8)
+
+    # aplica a formula da equalização para cada valor e atribui o
+    # valor obtido para os correspondentes da posição do valor original
+    for i in range(tamanhoh):
+        valor_eq = ((tamanhoh - 1) / float(x * y)) * histogramac[i]
+        equalizada[np.where(img == i)] = valor_eq
+
+    return equalizada
+
+
+# equaliza cada imagem individualmente e retorna os resuldados
+def equalizacao_histograma_individual(img1, img2, img3, img4):
+    equalizada1 = _equaliza_histograma_individual(img1)
+    equalizada2 = _equaliza_histograma_individual(img2)
+    equalizada3 = _equaliza_histograma_individual(img3)
+    equalizada4 = _equaliza_histograma_individual(img4)
+
+    return (equalizada1, equalizada2, equalizada3, equalizada4)
+
+
+def equaliza_histograma_conjunto(img1, img2, img3, img4):
+    # calcula o histograma conjunto e constroi o histograma cumulativo conjunto
+    histograma = calcula_histograma_conjunto(img1, img2, img3, img4)
+    histogramac = np.zeros(histograma.shape)
+    histogramac[0] = histograma[0]
+    tamanhoh = histograma.shape[0]
+    x, y = img1.shape
+
+    for i in range(1, tamanhoh):
+        histogramac[i] = histograma[i] + histogramac[i - 1]
+
+    # cria uma matriz para cada imagem equalizada
+    equalizada1 = np.zeros(img1.shape).astype(np.uint8)
+    equalizada2 = np.zeros(img1.shape).astype(np.uint8)
+    equalizada3 = np.zeros(img1.shape).astype(np.uint8)
+    equalizada4 = np.zeros(img1.shape).astype(np.uint8)
+
+    # em cada matriz nova, na posição onde na matriz original havia o
+    # valor descrito pela iteração, é posicionado o valor equalizado
+    for i in range(tamanhoh):
+        valor_eq = ((tamanhoh - 1) / float(x * y)) * histogramac[i]
+        equalizada1[np.where(img1 == i)] = valor_eq
+        equalizada2[np.where(img2 == i)] = valor_eq
+        equalizada3[np.where(img3 == i)] = valor_eq
+        equalizada4[np.where(img4 == i)] = valor_eq
+
+    return (equalizada1, equalizada2, equalizada3, equalizada4)
 
 
 def ajuste_gamma(img1, img2, img3, img4, gama):
@@ -44,9 +133,9 @@ def main():
     imghq = imageio.imread(nome_hq + '.png')
 
     if realce == 1:
-        histograma_transf_individual(img1, img2, img3, img4)
+        equalizacao_histograma_individual(img1, img2, img3, img4)
     elif realce == 2:
-        histograma_transf_conjunta(img1, img2, img3, img4)
+        equalizacao_histograma_conjunto(img1, img2, img3, img4)
     elif realce == 3:
         ajuste_gamma(img1, img2, img3, img4, gama)
 
