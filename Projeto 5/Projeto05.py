857a9f561c531e7ced743c1870f974fd44149e8b
carolesj <caroline.jesuino.silva@usp.br>
2018-05-02 00:03:04 -0300

    Trabalho finalizado

--- Projeto 5/Projeto05.py
+++ Projeto 5/Projeto05.py
@@ -17,19 +17,24 @@ def filtraimagem(g, filtro):
     limitef = (np.max(filtro) * 0.9)
     limiteg = (np.max(g) * 0.01)
 
-    np.where(g > limitef, g, 0)
-    np.where(g < limiteg, g, 0)
+    np.where(g >= limitef, g, 0)
+    np.where(g <= limiteg, g, 0)
 
-    return
+    #print("Filtrada\n")
+    return g
 
 
 'Faz uma convolução com filtro de média para suavização da imagem'
 def passamedia(g):
     x, y = g.shape
+    temp = np.zeros(g.shape)
     for i in range(x):
         for j in range(y):
-            g[i][j] = (sum(sum(g[i - 3 : i + 3, j - 3 : j + 3])))/49
-    return
+            temp[i][j] = np.real((np.sum(np.sum(g[i - 3 : ((i + 4) % x), j - 3 : ((j + 4) % y)])))/49)
+
+    g = np.copy(temp)
+    #print("Média passada\n")
+    return g
 
 
 'Normaliza os valores obtidos para que fiquem entre 0 e 255'
@@ -37,36 +42,48 @@ def normaliza(g):
     maximo = np.max(g)
     minimo = np.min(g)
 
-    g = (((maximo - g) / (maximo - minimo)) * 255).astype(np.uint8)
+    g = (((maximo - g) / (maximo - minimo)) * 255)
 
-    return g
+    #print("Normalizada\n")
+    return np.real(g).astype(np.uint8)
 
 
+'Aplica o filtro de GErchberg PApoulis, num momento de muito bom humor' \
+'para programar, apelidado de gepeto para ser mais facilmente chamado'
 def gepeto(imgi, imgm, T):
     filtro = np.fft.fft(imgm)
 
+    #print(T)
+    # para cada iteração solicitada
     for i in range(T):
+        # passa a fourier e filtra a imagem, voltando-a pro domínio do espaço depois
         g = np.fft.fft(imgi)
         filtraimagem(g, filtro)
         g = np.fft.ifft(g)
-        passamedia(g)
+        # convolui com um filtro de média
+        g = passamedia(g)
+        # normaliza o valor obtido
         g = normaliza(g)
+
         g = imgi * (1 - imgm / 255) + g * (imgm / 255)
 
+    #print("Gepetado\n")
     return g
 
 
 
 def rmse(imgo, imgr):
+    #print("Entrou no erro")
     erro = 0
     x, y = imgr.shape
 
     for i in range(x):
         for j in range(y):
-            erro += (imgo - imgr) ** 2
+            erro += (imgo[i][j] - imgr[i][j]) ** 2
 
     erro = np.sqrt((1 / (x * y)) * erro)
 
+    #print("Erro gerado\n")
     return erro
 
 
